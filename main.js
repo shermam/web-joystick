cd8b76d99a67764fd6128ff2f2a0ce4fde56d189
shermam.miranda <shermam.miranda@hotmail.com>
2017-12-15 16:06:23 -0200

    ajustes iniciais

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('oi');
+Array.from(document.querySelectorAll('button'))
+    .forEach(b => {
+        // b.addEventListener('touchmove', e => {
+        //     e.stopPropagation();
+        //     e.preventDefault();
+        //     navigator.vibrate(50);
+        // });
+
+        b.addEventListener('touchstart', e => {
+            e.stopPropagation();
+            e.preventDefault();
+            navigator.vibrate(50);
+        });
+
+    });

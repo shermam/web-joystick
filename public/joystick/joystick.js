62353ab1cef1f8ec476e3c240beea4558aad5b97
Shermam Miranda <shermammiranda@Shermams-MacBook-Pro.local>
2017-12-17 19:28:25 -0200

    sending more info on the event

--- public/joystick/joystick.js
+++ public/joystick/joystick.js
@@ -4,13 +4,19 @@ var con = connection('joystickPage', 'gamePage', console.log);
 
 Array.from(document.querySelectorAll('.action-button'))
     .forEach(b => {
-        ['touchenter', 'touchstart', 'click']
+        ['touchstart', 'touchend']
             .forEach(evName => {
                 b.addEventListener(evName, e => {
                     e.stopPropagation();
                     e.preventDefault();
                     navigator.vibrate(50);
-                    con.peer.send(e.target.id);
+
+                    const data = JSON.stringify({
+                        event: evName,
+                        code: e.target.id
+                    });
+                    
+                    con.peer.send(data);
                 });
             });
     });
--- public/main.js
+++ public/main.js
@@ -1,3 +1,18 @@
 import connection from "./connection.js";
 
-connection('gamePage', 'joystickPage', data => document.querySelector('h1').innerHTML = data);
+const evtTypes = {
+    'touchstart' : 'keydown', 
+    'touchend' : 'keyup'
+}
+
+connection('gamePage', 'joystickPage', data => {
+    data = JSON.parse(data);
+    document.querySelector('h1').innerHTML = data;
+    console.log(data);
+  
+    window.dispatchEvent(new KeyboardEvent(evtTypes[data.event], {
+        code: data.code
+    }));
+  
+});
+

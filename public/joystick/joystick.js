1654b6074c04edd9ea07bd45b1589852c0e15d3b
Shermam Miranda <shermammiranda@Shermams-MacBook-Pro.local>
2017-12-17 01:22:00 -0200

    Comunication

---
+++ public/joystick/joystick.js
@@ -0,0 +1,69 @@
+const auth = firebase.auth();
+var p = null;
+
+Array.from(document.querySelectorAll('.action-button'))
+    .forEach(b => {
+        ['touchenter', 'touchstart', 'click']
+            .forEach(evName => {
+                b.addEventListener(evName, e => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    navigator.vibrate(50);
+                    console.log(e.target.id);
+
+                    p.send(e.target.id);
+                });
+            });
+    });
+
+
+auth.onAuthStateChanged(user => {
+    if (!user) { return; }
+
+    const userData = firebase.database().ref('users/' + user.uid);
+    
+    
+    document.querySelector('#connect')
+        .addEventListener('click', e => {
+            
+            console.log('connect');
+
+            userData.set({
+                gamePage : 0,
+                joystickPage : 0
+            });
+
+            p = new SimplePeer({ initiator: true });
+            
+            p.on('error', function (err) { console.log('error', err) })
+            
+            p.on('signal', function (data) {
+                console.log('SIGNAL', JSON.stringify(data))
+                userData.update({ gamePage: data });
+            })
+
+            userData.on('value', snapShot => {
+                const value = snapShot.val();
+                if (!value || !value.joystickPage) { return }
+                console.log(value);
+                p.signal(value.joystickPage);
+                //userData.update({ joystickPage: 0 });
+            });
+
+            p.on('connect', function () {
+                console.log('CONNECT')
+                userData.update({ joystickPage: 0, gamePage : 0 });
+                p.send('whatever' + Math.random())
+            })
+
+            p.on('data', function (data) {
+                console.log('data: ' + data)
+            })
+
+        });
+});
+
+document.querySelector('#login')
+    .addEventListener('click', e => {
+        auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    });
--- public/main.js
+++ public/main.js
@@ -1,19 +1,41 @@
-Array.from(document.querySelectorAll('.action-button'))
-    .forEach(b => {
-        ['touchenter', 'touchstart']
-            .forEach(evName => {
-                b.addEventListener(evName, e => {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    navigator.vibrate(50);
-                });
-            });
+const auth = firebase.auth();
+const h1 = document.querySelector('h1');
+
+auth.onAuthStateChanged(user => {
+    if (!user) { return;}
+    
+    const userData = firebase
+    .database()
+    .ref('users/' + user.uid);
+
+    const p = new SimplePeer();
+    p.on('error', function (err) { console.log('error', err) })
+    
+    p.on('signal', function (data) {
+        console.log('SIGNAL', JSON.stringify(data))
+        userData.update({ joystickPage: data });
+    })
+
+    userData.on('value', snapShot => {
+        const value = snapShot.val();
+        if (!value || !value.gamePage) {return}
+        console.log(value);
+        p.signal(value.gamePage);
+        //userData.update({ gamePage: 0 });
     });
+
+    p.on('connect', function () {
+        console.log('CONNECT')
+        p.send('whatever' + Math.random())
+    })
+
+    p.on('data', function (data) {
+        console.log('data: ' + data)
+        h1.innerHTML = data;
+    })
+});
 
 document.querySelector('#login')
     .addEventListener('click', e => {
-        var provider = new firebase.auth.GoogleAuthProvider();
-        firebase.auth()
-            .signInWithPopup(provider)
-            .then(console.log);
+        auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
     });
